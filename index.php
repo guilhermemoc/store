926aeddbf24d61993193751323816f1704a710a8
Guilherme Martins <adm.gms@gmail.com>
2016-08-16 17:53:27 -0300

    .

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include_once('header.php') ?>
+<main>
+  <section class="desta">
+    <ul>
+      <li class="destaq"></li>
+      <li class="destaq"></li>
+      <li class="destaq"></li>
+      <li class="destaq"></li>
+      <li class="destaq"></li>
+      <li class="destaq"></li>
+      <li class="destaq"></li>
+      <li class="destaq"></li>
+<!--  <img src="http://lorempixel.com/output/cats-q-c-300-300-10.jpg" alt="" /> -->
+    </ul>
+  </section>
+
+  <section class="products-main">
+    <section class="produto">
+
+    </section>
+  </section>
+
+
+</main>
+<?php include_once('footer.php') ?>
