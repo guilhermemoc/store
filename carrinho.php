8a6d7796e608c26352deae8830d5091fbfedbb8b
Guilherme Martins <adm.gms@gmail.com>
2016-08-18 17:34:00 -0300

    .

--- carrinho.php
+++ carrinho.php
@@ -1,44 +1,62 @@
 <?php include_once('header.php') ?>
 <main>
-  <section class="container-main">
 
-    <section id="cart">
-			<article class="product">
-				<header>
-					<a class="remove">
-						<img src="http://www.astudio.si/preview/blockedwp/wp-content/uploads/2012/08/1.jpg" alt="">
-
-						<h3>Remove product</h3>
-					</a>
-				</header>
-
-				<div class="content">
-
-					<h1>Lorem ipsum</h1>
-
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, numquam quis perspiciatis ea ad omnis provident laborum dolore in atque.
-
-					<div title="You have selected this product to be shipped in the color yellow." style="top: 0" class="color yellow"></div>
-					<div style="top: 43px" class="type small">XXL</div>
-				</div>
-
-				<footer class="content">
-					<span class="qt-minus">-</span>
-					<span class="qt">2</span>
-					<span class="qt-plus">+</span>
-
-					<h2 class="full-price">
-						29.98€
-					</h2>
-
-					<h2 class="price">
-						14.99€
-					</h2>
-				</footer>
-			</article>
-
-  </section>
-
-
-</main>
-<?php include_once('footer.php') ?>
+  <head>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,700italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+  </head>
+  <section class="center-content">
+    <section class="product-container p-border">
+      <ul>
+        <li class="p-thumb">
+          <img src="https://static.wmobjects.com.br/imgres/arquivos/ids/2526999-164-164" alt="" />
+        </li>
+        <section class="p-box-title">
+          <li class="p-title">
+            produto de teste 010101
+          </li>
+          <li class="p-descr">
+            descrição do produto de teste para marcação de texto apenas
+          </li>
+        </section>
+        <li class="p-price">
+          Preço
+          <p>R$269.65</p>
+        </li>
+        <li class="p-quant">
+        </li>
+      </ul>
+      <ul>
+        <li class="p-thumb">
+          <img src="https://static.wmobjects.com.br/imgres/arquivos/ids/2526999-164-164" alt="" />
+        </li>
+        <section class="p-box-title">
+          <li class="p-title">
+            produto de teste 010101
+          </li>
+          <li class="p-descr">
+            descrição do produto de teste para marcação de texto apenas
+          </li>
+        </section>
+        <li class="p-price">
+          Preço
+          <p>R$269.65</p>
+        </li>
+        <!-- <li class="p-button">
+        <button>Confirmar</button>
+      </li> -->
+    </ul>
+    <div class="cep-contain">
+      <input class="input-cep" pattern="[0-9],[0-9][0-9]" value="Digite o CEP" onclick="javascript:this.value=''" type="text">
+      <li class="c-button">
+        <a href="del-page.php"><button>ok</button></a>
+      </li>
+      <div>
+      </section>
+      <section class="center-1024">
+        <button type="button" class="btn" name="button"></button>
+      </section>
+      <section/>
+      <!--a pen by Guilherme Martins (adm.gms@gmail.com)-->
+    </main>
+    <?php include_once('footer.php') ?>
